c78cd4bf96655f822a24b855af7850c4a9a487c4
fadybedir <70530044+fadybedir@users.noreply.github.com>
2020-09-02 20:28:32 +0200

    Update chapter1-2-step12.py

--- chapter1-2-step12.py
+++ chapter1-2-step12.py
@@ -20,12 +20,18 @@ def better_freq(text, k):
             freq_map[pattern] += 1
 
     
-    maxMap = max(freq_map.values())     #variable assigned to the max value in the dict
+    def maxMap(freq_map):                   # a function to determine the max value in a dictionnary
+        max_value = 0 
+        for key, value in freq_map.items():
+            if value > max_value:
+                max_value = value
+        return max_value
+    
     for pattern in freq_map:            #for loop that checks the value every key in the dict,
         if freq_map[pattern] == maxMap: #if it matches the max value, the key is added to a list 
             freq_pattern.append(pattern)
     
     print("the pattern is "+ str(freq_pattern))
-    print("it was repeated " + str(maxMap) + " times")
+    print("it was repeated " + str(maxMap(freq_map)) + " times")
 
 better_freq(text, k)
