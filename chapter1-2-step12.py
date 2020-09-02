a74399f4ee2331f943f10b4d9c4e0a15363d3fdb
fadybedir <70530044+fadybedir@users.noreply.github.com>
2020-09-02 07:34:14 +0200

    Add files via upload

---
+++ chapter1-2-step12.py
@@ -0,0 +1,31 @@
+# we have a string of text and the length of the pattern, and we figure out the frequent patterns or k-mers
+
+file = open('/users/fadygamal/dataset_3.txt', 'r')
+text = file.read()
+lines = text.split('\n')
+
+text = lines[0]
+n = len(text)
+k = 11
+
+freq_map = {}
+freq_pattern = []
+
+def better_freq(text, k):
+    for i in range(0, n):               #for loop that creates a key in the dict and assigns a value of 1 
+        pattern = text[i : i + k]       #if the key is already they, loop adds 1 to its value
+        if pattern not in freq_map:
+            freq_map[pattern] = 1
+        else: 
+            freq_map[pattern] += 1
+
+    
+    maxMap = max(freq_map.values())     #variable assigned to the max value in the dict
+    for pattern in freq_map:            #for loop that checks the value every key in the dict,
+        if freq_map[pattern] == maxMap: #if it matches the max value, the key is added to a list 
+            freq_pattern.append(pattern)
+    
+    print("the pattern is "+ str(freq_pattern[0]))
+    print("its composed of " + str(maxMap) + " nucleotides")
+
+better_freq(text, k)
