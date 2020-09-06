b806278722e10fd2e3d17e7ee3de2fa49aa458b5
fadybedir <70530044+fadybedir@users.noreply.github.com>
2020-09-06 22:00:05 +0200

    Add files via upload

---
+++ chapter1-8-step6.py
@@ -0,0 +1,25 @@
+#ApproximatePatternCount
+
+from hummingdistance import hum_dist
+
+file = open('/users/fadygamal/data-ch-1-8-step6.txt', 'r')
+txt = file.read()
+lines = txt.split('\n')
+
+pattern = lines[0]
+text = lines[1]
+d = int(lines[2])
+
+k = len(pattern)
+n = len(text)
+
+def app_patt_count(pattern, text, d):
+    count = 0
+    for i in range(n - k + 1):
+        patt = text[i : i + k]
+        humm = hum_dist(pattern, patt)
+        if humm <= d:
+            count += 1
+    return count
+
+print(app_patt_count(pattern, text, d))
---
+++ hummingdistance.py
@@ -0,0 +1,25 @@
+# find the hamming distance
+# the hamming distance is the number of mismatches between 2 strings 
+
+file = open('/users/fadygamal/data-ch-1-8-step3.txt', 'r')
+text = file.read()
+lines = text.split('\n')
+
+
+string1 = lines[0]
+string2 = lines[1]
+
+def hum_dist(string1, string2):
+    count = 0
+    for i in range(len(string1)):
+        if string1[i] != string2[i]:
+            count += 1
+    return count
+
+
+        
+
+
+
+
+
